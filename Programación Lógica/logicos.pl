686c69b868a50893a3150f395fc842e7e84b76b4
Zabai Armas Herrera <zbrmas@hotmail.com>
2017-05-11 16:45:49 +0100

    Prolog finished

--- Programación Lógica/logicos.pl
+++ Programación Lógica/logicos.pl
@@ -8,7 +8,7 @@ fibo(X, Y) :- X>1,
               Y is Y1+Y2.
 
 expo(_, 0, 1).
-expo(B, E, V) :- E > 0, !, E1 is E -1, pow(B,E1,V1), V is B * V1.
+expo(B, E, V) :- E > 0, !, E1 is E -1, expo(B,E1,V1), V is B * V1.
 
 minimo([V], V).
 minimo([X,Y|R], V):-
@@ -18,19 +18,28 @@ minimo([X,Y|R], V):-
         minimo([X|R], V)
     ).
 
-% WIP
-%inserta([], V, [V]).
-%inserta([C|R1], V, [C|R2]) :- (C >.
+inserta([], A, [A]).
+inserta([X|R], A, [X|R]) :- A =:= X, !.
+inserta([X|R], A, [A,X|R]) :- A < X.
+inserta([X|R1], A, [X|R2]) :- inserta(R1, A, R2).
 
 invierte(L, R):-  invierteAux(L, [], R).
 invierteAux([], V, V).
 invierteAux([C|Rl], V, R):-  invierteAux(Rl, [C|V], R).
 
-% WIP
-%elimina([], V, []).
-%elimina([C|R], V, [C|R]) :- ( C =:= V -> elimina(R, V, R); elimina(R, V, [C|R]) ).
+elimina([], _, []) :- !.
+elimina([H|T], V, Result) :- H=:=V, elimina(T, V, Result), !.
+elimina([H|T], V, [H|Result]) :- elimina(T, V, Result).
 
+repetidos([],[]).
+repetidos([H|T],[H|Out]) :-
+    not(esta(H,T)),
+    repetidos(T,Out).
+repetidos([H|T],Out) :-
+    esta(H,T),
+    repetidos(T,Out), !.
+    
+esta(X,[X|_]).
+esta(X,[_|T]) :- esta(X,T).
 
-test(V, V).
-%test([C|R], [C|R2]).
-%padre(frank, ana).
+not(A) :- \+ call(A).
